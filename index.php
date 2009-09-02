2241f60197179420d0a41c1ae3447d65220182be
Jade Robbins <jaderobbins@gmail.com>
2009-09-02 13:53:45 -0600

    removed the php includes

--- index.php
+++ index.php
@@ -1,97 +1,198 @@
-<!-- Web Template: 1cdn version 0.13p -->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp1.php"); ?>
-<? bloginfo('title'); ?>
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp2.php"); ?>
+<!-- Web Template: 1cdn version 0.14n -->
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en-US">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<!-- END BRANDED CONTENT DO NOT EDIT -->
+  <title><? bloginfo('title'); ?></title>
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+  <script type="text/javascript" language="javascript" src="http://www.montana.edu/msucommon/java/globalJava.js"></script>
+  <script type="text/javascript" language="javascript" src="http://www.montana.edu/msucommon/java/styleswitcher.js"></script>
 
-<!-- You may insert necessary includes here -->
-<meta HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<meta HTTP-EQUIV="Expires" CONTENT="-1">
-<link href="<? echo bloginfo('template_url') ?>/style.css" type="text/css" rel="stylesheet" title="styles">
-<link href="feed/" rel="alternate" type="application/rss+xml" title="Sitewide RSS Feed" />
-<!--[if lt IE 7]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE7.js" type="text/javascript"></script>
-<![endif]-->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp3.php"); ?>
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp4.php"); ?>
-<!---------------------------------------->
-<!-- Begin Insert Left Nav Content-->
-<!---------------------------------------->
-<div class="sidebar" id="sidebarLeft">
-<ul>
-	<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) : ?>
-	<?php endif; ?>
-</ul>
-</div>
-<!---------------------------------------->
-<!-- End Insert Other Left Nav Content  -->
-<!---------------------------------------->
-<? get_header(); ?>
+  <link href="http://www.montana.edu/msucommon/css/htmlstyles.css" type="text/css" rel="stylesheet" title="styles">
+  <link href="http://www.montana.edu/msucommon/css/styles.css" type="text/css" rel="stylesheet" title="styles">
+  <link href="http://www.montana.edu/msucommon/css/design.css" type="text/css" rel="stylesheet" title="styles">
+  <link href="http://www.montana.edu/msucommon/css/textonly.css" type="text/css" rel="alternate stylesheet" title="textonly">
+  <link href="http://www.montana.edu/msucommon/css/htmlstyles.css" type="text/css" rel="stylesheet" title="textonly">
+  <link href="<? echo bloginfo('template_url') ?>/style.css" type="text/css" rel="stylesheet" title="styles">
+  <link href="feed/" rel="alternate" type="application/rss+xml" title="Sitewide RSS Feed" />
+  <meta HTTP-EQUIV="Pragma" CONTENT="no-cache">
+  <meta HTTP-EQUIV="Expires" CONTENT="-1">
+  <!--[if lt IE 7]>
+  <script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE7.js" type="text/javascript"></script>
+  <![endif]-->
+ </head>
+ <body class="MSUbodyfixed" id="MSUbodyfixed">
+
+  <div id="MSUtextheader">
+   <h3><span class="MSUtextheaderMSU">Montana State University</span><br><a href="http://www.montana.edu/wwwprov/">Academics</a> | <a href="http://www.montana.edu/level2/admin.html">Administration</a> | <a href="http://www.montana.edu/level2/prospectivestudents.php">Admissions</a> | <a href="http://www.montana.edu/siteindex/">A-Z Index</a> | <a href="http://www.montana.edu/msuinfo/dx/">Directories</a></h3>
+
+  </div>
+  <table cellpadding="0" cellspacing="0" class="MSUfixed">
+   <tr>
+    <td valign="top">
+<!----------------> 
+<!--Begin Header-->
+<!---------------->
+     <table cellpadding="0" cellspacing="0" id="MSUheader" class="MSUfixed"><tr><td align="center"><form method="get" style="display: block; display:inline" action="http://www.montana.edu/search/"><table bgcolor="#001155" width="760" cellpadding="0" cellspacing="0" border="0"><tr>
+       <td rowspan="2" class="MSUheadersearch" width="249" valign="bottom" nowrap><a href="http://www.montana.edu/"><img src="http://www.montana.edu/msucommon/images/msulogo.jpg" alt="Montana State University" border="0"></a><img src="http://www.montana.edu/msucommon/images/msulogo2.jpg" alt="spacer"></td>
+       <td align="left" class="MSUheadersearch" width="336" valign="bottom" nowrap><img src="http://www.montana.edu/msucommon/images/mountainsandminds.jpg" alt="Mountains and Minds"></td>
+
+       <td align="right" class="MSUheadersearch" width="134" nowrap><input type="hidden" name="cx" value="008992511269271655684:zana6q1rfbs"><input type="hidden" name="cof" value="FORID:11"><input type="text" name="q" size="13" class="MSUheadersearchbox" value="Search MSU" alt="Search MSU"  onFocus="if (this.value == 'Search MSU') this.value = '';"></td>
+       <td align="center" class="MSUheadersearch"  width="41" valign="middle" nowrap><input type="image" src="http://www.montana.edu/msucommon/images/gobtn.jpg" align="bottom" alt="Search" name="sa" class="MSUsubmitBtn"></td>
+      </tr>
+      <tr>
+       <td align="left" width="511" valign="bottom" nowrap colspan="3"><a href="http://www.montana.edu/wwwprov/" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('academics','','http://www.montana.edu/msucommon/images/academics-on.jpg',1)"><img src="http://www.montana.edu/msucommon/images/academics.jpg" alt="MSU Academics" name="academics" width="71" height="26" border="0"></a><img src="http://www.montana.edu/msucommon/images/spacer1.jpg" width="19" height="26" alt="spacer"><a href="http://www.montana.edu/level2/admin.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('administration','','http://www.montana.edu/msucommon/images/administration-on.jpg',1)"><img src="http://www.montana.edu/msucommon/images/administration.jpg" alt="MSU Administration" name="administration" width="105" height="26" border="0"></a><img src="http://www.montana.edu/msucommon/images/spacer2.jpg" width="19" height="26" alt="spacer"><a href="http://www.montana.edu/level2/prospectivestudents.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('admissions','','http://www.montana.edu/msucommon/images/admissions-on.jpg',1)"><img src="http://www.montana.edu/msucommon/images/admissions.jpg" alt="MSU Admissions" name="admissions" width="76" height="26" border="0"></a><img src="http://www.montana.edu/msucommon/images/spacer3.jpg" width="19" height="26" alt="spacer"><a href="http://www.montana.edu/siteindex/" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('atozindex','','http://www.montana.edu/msucommon/images/atozindex-on.jpg',1)"><img src="http://www.montana.edu/msucommon/images/atozindex.jpg" alt="MSU A-Z Index" name="atozindex" width="64" height="26" border="0"></a><img src="http://www.montana.edu/msucommon/images/spacer4.jpg" width="19" height="26" alt="spacer"><a href="http://www.montana.edu/msuinfo/dx/" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('directories','','http://www.montana.edu/msucommon/images/directories-on.jpg',1)"><img src="http://www.montana.edu/msucommon/images/directories.jpg" alt="MSU Directories" name="directories" width="78" height="26" border="0"></a><img src="http://www.montana.edu/msucommon/images/msulogo3.jpg" alt="spacer"></td>
+      </tr>
+     </table><noscript><div align="center"><a href="http://www.montana.edu/wwwprov/">Academics</a> | <a href="http://www.montana.edu/level2/admin.html">Administration</a> | <a href="http://www.montana.edu/level2/prospectivestudents.php">Admissions</a> | <a href="http://www.montana.edu/siteindex/">A-Z Index</a> | <a href="http://www.montana.edu/msuinfo/dx/">Directories</a></div></noscript></form></td></tr></table>
+<!---------------->
+<!-- End Header -->
+
+<!---------------->
+     <table id="MSUfixed2" class="MSUfixed2" width="100%" align="center" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+       <td id="MSUmain">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+         <tr>
+          <td width="150" valign="top" id="MSUnavbar" class="MSUnavbar">
+           <div class="MSUnavbar">&nbsp;</div>
+            <div class="sidebar" id="sidebarLeft">
+            <ul>
+              <?php   /* Widgetized sidebar, if you have the plugin installed. */
+                if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) : ?>
+              <?php endif; ?>
+            </ul>
+            </div>
+           </div>
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+          </td>
+          <td width="420" rowspan="2" valign="top" id="MSUcontent">
+<!-- END BRANDED CONTENT DO NOT EDIT -->
+
+<!------------------------>
+<!-- Insert Header Image-->
+<!------------------------>
+<div id="headerImageContainer"><span id="pageTitle"><a href="<? bloginfo('url') ?>"><? bloginfo('name'); ?></a></span></div>
+<!---------------------------->
+<!-- End Insert Header Image-->
+<!---------------------------->
+
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+           <table cellpadding="0" cellspacing="0" border="0">
+            <tr>
+             <td valign="top" class="MSUcontent-dn">
+<!-- END BRANDED CONTENT DO NOT EDIT -->
+
+<!-------------------->
+<!--Department Title-->
+<!-------------------->
+<a href="http://www.montana.edu/commserv" class="departmentTitle">&gt; Office of Communications and Public Affairs</a>
+<!---------------->
+<!-- Page Title -->
+
+<!---------------->
+<div class="pageTitle">Office of Communications and Public Affairs</div>
 <!-------------------------------->
 <!-- Main Content Window Begin- -->
 <!-------------------------------->
+
 <?php if (have_posts()) : ?>
 
-		<?php while (have_posts()) : the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
 
-			<div class="post" id="post-<?php the_ID(); ?>">
-				<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-				<br /><small><?php the_time('F jS, Y') ?> - Posted by <? the_author() ?></small>
+      <div class="post" id="post-<?php the_ID(); ?>">
+        <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+        <br /><small><?php the_time('F jS, Y') ?> - Posted by <? the_author() ?></small>
 
-				<div class="entry">
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
-				</div>
+        <div class="entry">
+          <?php the_content('Read the rest of this entry &raquo;'); ?>
+        </div>
                 
-				<p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?> <a href="<? comments_link() ?>"><?php comments_number('No Comments', '1 Comment', '% Comments', 'number'); ?></a></p>
-			</div>
+        <p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?> <a href="<? comments_link() ?>"><?php comments_number('No Comments', '1 Comment', '% Comments', 'number'); ?></a></p>
+      </div>
 
-		<?php endwhile; ?>
+    <?php endwhile; ?>
          
 
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		</div>
+    <div class="navigation">
+      <div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+      <div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+    </div>
 
-	<?php else : ?>
+  <?php else : ?>
 
-		<h2 class="center">Not Found</h2>
-		<p class="center">Sorry, but you are looking for something that isn't here.</p>
-		<?php include (TEMPLATEPATH . "/searchform.php"); ?>
+    <h2 class="center">Not Found</h2>
+    <p class="center">Sorry, but you are looking for something that isn't here.</p>
+    <?php include (TEMPLATEPATH . "/searchform.php"); ?>
 
-	<?php endif; ?>
-
-	</div>
-
+  <?php endif; ?>
 <!---------------------------->
 <!-- End Main Content Window-->
 <!---------------------------->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp9.php"); ?>
-<!-- ###### BEGIN OPTIONAL ITEM: Text-only / Last Updated ###### -->
-<!-- require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp10v2.php");-->
- 
-<!--require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp10v3.php"); -->
-<!-- ###### END OPTIONAL ITEM: Text-only / Last Updated  ##### -->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp11.php"); ?>
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+              <table id="MSUcontentfooter" width="100%" border="0" cellpadding="0" cellspacing="0">
+
+<!-- END BRANDED CONTENT DO NOT EDIT -->
+               <tr>
+          <td valign="bottom" align="left" id="MSUregviewlink" nowrap><a href="http://www.montana.edu/msucommon/css/styles.css" onclick="setActiveStyleSheet('styles'); return false;">Return to regular view</a></td>
+                <td valign="bottom" align="left" class="MSUtextOnly" id="MSUtextOnly"><a href="http://www.montana.edu/msucommon/css/textonly.css" onclick="setActiveStyleSheet('textonly'); return false;"><img src="http://www.montana.edu/msucommon/images/textonly.jpg" border="0" alt="View Text-only Version" align="bottom" vspace="0"></a>&nbsp;<a href="http://www.montana.edu/msucommon/css/textonly.css" onclick="setActiveStyleSheet('textonly'); return false;">Text-only</a></td>
+                <td valign="bottom" align="right" class="font11" nowrap>Updated:
 <!------------------------>
-<!--Insert Header2 Image-->
+<!-- Begin Update Date  -->
 <!------------------------>
-<!--<img src="" width="170" alt="">-->
-<!---------------------------->
-<!--End Insert Header2 Image-->
-<!---------------------------->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp11-5.php"); ?>
-<!-------------------------------->
-<!-- Begin Right Nav Bar Content-->
-<!-------------------------------->
-<div class="sidebar" id="sidebarRight">
-<ul>
-			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : ?>
-			<?php endif; ?>
-</ul>
-</div>
-<!-------------------------------->
-<!-- End Right Nav Bar Content  -->
-<!-------------------------------->
-<? require_once("http://www.montana.edu/msucommon/php/1cdn/1cdnp12.php"); ?>
+1/10/2006
+<!-------------------->
+<!-- End Update Date-->
+
+<!-------------------->
+                </td>
+               </tr>
+<!-- BEGIN BRANDED CONTENT DO NOT EDIT -->
+               <tr>
+          <td colspan="3"><img src="http://www.montana.edu/msucommon/images/whitedot.gif" height="1" width="390" alt="spacer"></td>
+         </tr>
+              </table>
+       </td>
+            </tr>
+
+           </table>
+          </td>
+          <td valign="top" width="170" id="MSUdnbar">
+           <table border="0" cellpadding="0" cellspacing="0" width="170">
+            <tr>
+             <td class="MSUdnbar">
+              <div class="sidebar" id="sidebarRight">
+              <ul>
+                    <?php   /* Widgetized sidebar, if you have the plugin installed. */
+                      if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : 
+                    endif; ?>
+              </ul>
+              </div>
+             </td>
+            </tr>
+           </table>
+          </td>
+         </tr>
+         <tr id="MSUbottomBar">
+          <td class="MSUbgGold" width="170" valign="bottom"><img src="http://www.montana.edu/msucommon/images/golddot.gif" height="1" width="170" alt="spacer"></td>
+          <td class="MSUbgLTBlue" width="170" valign="bottom"><img src="http://www.montana.edu/msucommon/images/bluedot.gif" height="1" width="170" alt="spacer"></td>
+         </tr>
+        </table>
+
+       </td>
+      </tr>
+     </table>
+     <table id="MSUfooter-table" class="MSUfooter-table" align="center" width="760" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+       <td class="MSUfooter-td" valign="top" align="left">&copy; Montana State University 2006</td>
+       <td class="MSUfooter-td" valign="top" align="right">Didn't Find it? Please use our <a href="http://www.montana.edu/html/contact.html">contact list</a> or our <a href="http://www.montana.edu/siteindex/">site index</a>.</td>
+
+      </tr>
+     </table>
+    </td>
+   </tr>
+  </table>
+ </body>
+</html>
